d99ac38937be5c85ed4a1098bcb93526d996a4de
Avik Roy <somavik99@gmail.com>
2023-07-14 23:22:04 +0530

    maib

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -39,51 +39,64 @@ const HomePage = () => {
   return (
     <>
       <div>
-        <NavBar />
+      
       </div>
+      <NavBar />
       <span className="Home__cont">
+      
         <div className="Img__cont">
           {AllImage.map((img, index) => {
             return (
               <div key={index}>
                 {index === Current && (
-                  <img src={img.ImageC} alt="Chair Image" className="Chair__image"/>
+                  <img
+                    src={img.ImageC}
+                    alt="Chair Image"
+                    className="Chair__image"
+                  />
                 )}
               </div>
             );
           })}
         </div>
-       <div className="Desc__cont">
-        <div>
-          <span
-            className="Text__cont"
-            style={{ fontWeight: "900", fontSize: "44px" }}
-          >
-            Discover innovative ways to decorate
-          </span>
+        <>
+        <button
+          style={{ background: "black" }}
+          onClick={() => HandleClickPrev(Current)}
+          className="btn__img"
+        >
+          <img src={LeftArrow} alt="" />
+        </button>
+        <button
+          style={{ background: "black" }}
+          onClick={() => HandleClickNext(Current)}
+          className="btn__img"
+        >
+          <img src={RightArrow} alt="" />
+        </button>
+        </>
+        <div className="Desc__cont">
           <div>
-            <span>
-              We provide unmatched quality , comfort, and style for property
-              owners across the country. Our experts combine form and function
-              in bringing vision to life. Create a room in your own style with
-              our collection and make a property a reflection of you and what
-              you love.
+            <span
+              className="Text__cont"
+              style={{ fontWeight: "900", fontSize: "44px" }}
+            >
+              Discover innovative ways to decorate
             </span>
-            <button>SHOP NOW</button>
+            <div>
+              <span
+                style={{ color: "white", fontSize: "12px", fontWeight: "900" }}
+              >
+                We provide unmatched quality , comfort, and style for property
+                owners across the country. Our experts combine form and function
+                in bringing vision to life. Create a room in your own style with
+                our collection and make a property a reflection of you and what
+                you love.
+              </span>
+              <br />
+              <div  className="Shop__btn">S H O P   N O W <span style={{fontSize:"50px",color: "#ff0000"}}>&#8594;</span> </div>
+            </div>
           </div>
-        </div>  
-          <button
-            style={{ background: "black" }}
-            onClick={() => HandleClickPrev(Current)}
-          >
-            <img src={LeftArrow} alt="" />
-          </button>
-          <button
-            style={{ background: "black" }}
-            onClick={() => HandleClickNext(Current)}
-          >
-            <img src={RightArrow} alt="" />
-          </button>
         </div>
       </span>
     </>
