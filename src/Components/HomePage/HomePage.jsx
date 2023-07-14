8811c4c21476e2880f2c858f69dd9cf9a4296a47
Avik Roy <somavik99@gmail.com>
2023-07-14 13:45:22 +0530

    main

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -41,46 +41,51 @@ const HomePage = () => {
       <div>
         <NavBar />
       </div>
-
-      <div className="Img__cont">
-        {AllImage.map((img, index) => {
-          return (
-            <div key={index}>
-              {index === Current && <img src={img.ImageC} alt="Chair Image" />}
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        <button
-          style={{ background: "black" }}
-          onClick={() => HandleClickPrev(Current)}
-        >
-          <img src={LeftArrow} alt="" />
-        </button>
-        <button
-          style={{ background: "black" }}
-          onClick={() => HandleClickNext(Current)}
-        >
-          <img src={RightArrow} alt="" />
-        </button>
-        <span
-          className="Text__cont"
-          style={{ fontWeight: "900", fontSize: "44px" }}
-        >
-          Discover innovative ways to decorate
-        </span>
-        <div>
-          <span>
-            We provide unmatched quality , comfort, and style for property
-            owners across the country. Our experts combine form and function in
-            bringing vision to life. Create a room in your own style with our
-            collection and make a property a reflection of you and what you
-            love.
-          </span>
-          <button>SHOP NOW</button>
+      <span className="Home__cont">
+        <div className="Img__cont">
+          {AllImage.map((img, index) => {
+            return (
+              <div key={index}>
+                {index === Current && (
+                  <img src={img.ImageC} alt="Chair Image" className="Chair__image"/>
+                )}
+              </div>
+            );
+          })}
         </div>
-      </div>
+       <div className="Desc__cont">
+        <div>
+          <span
+            className="Text__cont"
+            style={{ fontWeight: "900", fontSize: "44px" }}
+          >
+            Discover innovative ways to decorate
+          </span>
+          <div>
+            <span>
+              We provide unmatched quality , comfort, and style for property
+              owners across the country. Our experts combine form and function
+              in bringing vision to life. Create a room in your own style with
+              our collection and make a property a reflection of you and what
+              you love.
+            </span>
+            <button>SHOP NOW</button>
+          </div>
+        </div>  
+          <button
+            style={{ background: "black" }}
+            onClick={() => HandleClickPrev(Current)}
+          >
+            <img src={LeftArrow} alt="" />
+          </button>
+          <button
+            style={{ background: "black" }}
+            onClick={() => HandleClickNext(Current)}
+          >
+            <img src={RightArrow} alt="" />
+          </button>
+        </div>
+      </span>
     </>
   );
 };
